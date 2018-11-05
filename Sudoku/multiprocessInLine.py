c2cea76b2608f09b0873485f10dd49011e821ef0
VenancioIgrejas <venancio.igrejas@poli.ufrj.br>
2018-11-04 23:48:57 -0200

    add better prints

--- Sudoku/multiprocessInLine.py
+++ Sudoku/multiprocessInLine.py
@@ -17,21 +17,28 @@ send,read = Pipe()
 lock = Lock()
 
 
-#list_process = [Process(target=create_lines,args=(q,event,read)) for i in range(3)]
-#list_process.append(Process(target=control_process,args=(q,event,send,lock)))
-#for process in list_process:
+# list_process = [Process(target=create_lines,args=(q,event,read)) for i in range(3)]
+# list_process.append(Process(target=control_process,args=(q,event,send,lock)))
+# for process in list_process:
 #    process.start()
-
-#for process in list_process:
+#
+# for process in list_process:
 #    process.join()
 
 
 
-# work but with the same time if it wasn't parallel 
+# work but with the same time if it wasn't parallel
 with timer('processo terminado'):
-    #p = Process(target=father_proc,args=(q,q_answer))
     p = Process(target=father_proc,args=(q,q_answer))
     p.start()
     p.join()
 
-    print q_answer.get()
+    mt = q_answer.get()
+
+# print the answer
+
+list_df = [pd.DataFrame(mt[i]) for i in range(3)] # transform each line of mt in DataFrame and put in list
+
+sudoku = pd.concat(list_df).values # transform the list of Dataframe in Dataframe matrix and return as np.array
+
+print(sudoku)
